548a72ac7d650431d3cef37fcecfd91d6c110d83
Sumit Jha <sumitjha1618@gmail.com>
2025-08-25 10:06:04 +0530

    I/O

---
+++ 01-language-basics/a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // declaration of variables
+    int length;  // 0
+    int breadth; // 0
+
+    // output(prompt on terminal) and input from user
+    cout << "Enter the length: ";
+    cin >> length;
+
+    cout << "Enter the breadth: ";
+    cin >> breadth;
+
+    // printing values of the variables
+    cout << "Length: " << length << endl;
+    cout << "Breadth: " << breadth << endl;
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+* open this directory in obsidian for better visuals 
+
+- contains the DSA and interview related learning 
+- makes appearing in interviews way easier 
+
